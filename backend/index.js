fb3a266b3a19e6b85c51b195efd033bbc4f000d6
jmmadubuike <josephmmadubuike@yahoo.com>
2024-10-30 12:09:48 +0100

    commit

--- backend/index.js
+++ backend/index.js
@@ -7,14 +7,12 @@ const cartRoutes = require("./routes/cartRoutes");
 const orderRoutes = require("./routes/orderRoutes");
 const adminRoutes = require("./routes/adminRoutes");
 
-dotenv.config(); // Load environment variables from .env file
+dotenv.config();
 
 const app = express();
 
-// Middleware to parse incoming JSON requests
 app.use(express.json());
 
-// Connect to the MongoDB database
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
@@ -34,7 +32,6 @@ app.use((err, req, res, next) => {
   res.status(500).send({ message: "Server error" });
 });
 
-// Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
